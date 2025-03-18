ba6e90fc6e20349af298506a3ec617c46dc5f80a
YogeshKumar-saini <yksaini0193@gmail..com>
2025-03-19 00:40:58 +0530

    initialize project structure with essential files and directories

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Welcome to the comprehensive documentation for our Fake Review Detection project. This project is designed to distinguish between computer-generated (fake) reviews and original (human-written) reviews by leveraging a variety of natural language processing (NLP) techniques, traditional machine learning models, and deep learning methods. In this document, we detail the project background, data processing, feature engineering, model experimentation, experiment tracking using MLflow, and finally, the deployment of our application',
+    author='Yogesh Kumar Saini  and Sumit',
+    license='',
+)
